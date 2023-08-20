2e661991d3ee3c7e9133b5375a7feb7e4e633a73
Brandon <brandon17.2018@gmail.com>
2023-08-20 20:11:57 -0300

    linhas1

--- Ded.js
+++ Ded.js
@@ -3,12 +3,12 @@ function sla(){
     clas = document.getElementById('classe')
     raca = document.getElementById('raca')
    
-    document.getElementById("escolhaclasse").innerHTML = `a raça do seu personagem é ${raca.options[raca.selectedIndex].text} e sua classe é  ${clas.options[clas.selectedIndex].text}`
-    escolhac = document.getElementById('escolhaclasse')
+    document.getElementById("escolha").innerHTML = `a raça do seu personagem é ${raca.options[raca.selectedIndex].text} e sua classe é  ${clas.options[clas.selectedIndex].text}`
+    escolha = document.getElementById('escolha')
     
 
-    escolhac.style.display ="block"
-    escolhar.style.display ="block"
+    escolha.style.display ="block"
+    
     /*alert(`a raca é ${raca.options[raca.selectedIndex].text} e a classe é  ${clas.options[clas.selectedIndex].text}`)*/
 
     
