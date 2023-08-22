2428a562de34a053072700feef1d353b4c01770b
Henrique Ribeiro <henrique9mil@gmail.com>
2023-08-22 20:57:49 -0300

    henrique
    henrique

--- Ded.js
+++ Ded.js
@@ -1,19 +1,30 @@
 $(function(){
+
     document.getElementById('sla').onclick = function(){
-    var classe = document.getElementById('classe');
-    console.log("a classe é " + classe.options[classe.selectedIndex].text);
-    var raca = document.getElementById('raca');
-    console.log("a raca é " + raca.options[raca.selectedIndex].text);
-
-    document.getElementById("escolhaclasse").innerHTML = classe.options[classe.selectedIndex].text;
-    document.getElementById("escolharaca").innerHTML = raca.options[raca.selectedIndex].text;
-
-    var escolha1 = document.getElementById('escolhaclasse')
-    var escolha2 = document.getElementById('escolharaca')
-
-    escolha1.style.display ="block"
-    escolha2.style.display ="block"
-
+        var classe = document.getElementById('classe');
+        var suaclasse = (classe.options[classe.selectedIndex].text);
+        var raca = document.getElementById('raca');
+        var suaraca = (raca.options[raca.selectedIndex].text);
+        var nclasse = Number(classe.options[classe.selectedIndex].value);
+        var nraca = Number(raca.options[raca.selectedIndex].value);
+        var escolha1 = document.getElementById('escolhaclasse');
+        var escolha2 = document.getElementById('escolharaca');
+        
+             
+        if(nclasse == 0 && nraca == 0){
+            alert("escolha uma classe e uma raça")
+        }
+        else if (nclasse == 0){
+            alert("escolha uma classe")
+        }else if (nraca == 0){
+            alert('escolha uma raça');
+        }else{
+            escolha1.innerHTML = suaclasse
+            escolha2.innerHTML = suaraca
+            escolha1.style.display ="block"
+            escolha2.style.display ="block" 
+        }
+            
 
     
 
